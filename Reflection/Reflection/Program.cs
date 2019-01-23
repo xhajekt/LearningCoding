79e3f312ab16cb0795e7bf801b8490ef9c88ff28
Tomas Hajek <tomas.hajek@eset.sk>
2019-01-23 12:26:53 +0100

    reflection - first

---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+            Type listType = typeof(List<int>);
+            Type[] parameterTypes = { typeof(int) };
+            MethodInfo addMethod = listType.GetMethod("Add", parameterTypes);
+            for (int i = 0; i < 9; i++)
+            {
+                addMethod.Invoke(list, new object[] { i });
+            }
+        }
+    }
+}
